fc67aa028d1feeccdd5924a483c680db2fa8cca6
Bentot23 <92287648+Bentot23@users.noreply.github.com>
2022-10-16 15:14:20 -0500

    first commit

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
 function PokemonPage() {
+  const [pokemon, setPokemon] = useState([]);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:3001/pokemon')
+    .then(r => r.json())
+    .then(data => setPokemon(data))
+  }, [])
+
+  const filteredPokemon = pokemon.filter(poke =>
+    poke.name.toLowerCase().includes(search.toLocaleLowerCase()))
+
+  const handleAddPokemon = (newPokemon) => {
+    setPokemon([newPokemon, ...pokemon])
+  }
+
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
       <br />
-      <PokemonForm />
+      <PokemonForm onAddPokemon={handleAddPokemon} />
       <br />
-      <Search />
+      <Search search={search} onChangeSearch={setSearch} />
       <br />
-      <PokemonCollection />
+      <PokemonCollection pokemon={filteredPokemon} />
     </Container>
   );
 }
